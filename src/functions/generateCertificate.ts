e7a9e215a23007160621fe4e19f88662ac8ad574
kielkow <matheuskiel@fiorifer.com.br>
2022-07-13 11:45:57 -0300

    feat: generate template of certificate

--- src/functions/generateCertificate.ts
+++ src/functions/generateCertificate.ts
@@ -1,10 +1,30 @@
 import { APIGatewayProxyHandler } from "aws-lambda"
+import { readFileSync } from "fs";
+import { join } from "path";
 import { document } from '../utils/dynamodbClient'
+import * as handlebars from 'handlebars';
+import dayjs from "dayjs";
 
 interface ICreateCertificate {
     id: string;
     name: string;
     grade: string;
+}
+
+interface ITemplate {
+    id: string;
+    name: string;
+    grade: string;
+    medal: string;
+    date: string;
+}
+
+const compileTemplate = async (data: ITemplate) => {
+    const filePath = join(process.cwd(), "src", "templates", "certificate.hbs");
+
+    const html = readFileSync(filePath, "utf-8");
+
+    return handlebars.compile(html)(data)
 }
 
 export const handler: APIGatewayProxyHandler = async (event) => {
@@ -27,6 +47,21 @@ export const handler: APIGatewayProxyHandler = async (event) => {
             ":id": id
         }
     }).promise();
+
+    const medalPath = join(process.cwd(), "src", "templates", "selo.png");
+    const medal = readFileSync(medalPath, "base64");
+
+    const data: ITemplate = {
+        id,
+        name,
+        grade,
+        medal,
+        date: dayjs().format("DD/MM/YYYY")
+    }
+
+    const content = await compileTemplate(data);
+
+    console.log(content);
 
     return {
         statusCode: 201,
